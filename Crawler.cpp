f644b5641ae4946c2862924b8a0f829938e6d89e
shane-smyth <158466904+shane-smyth@users.noreply.github.com>
2025-04-01 22:53:11 +0100

    feature 4 now functional - fixed errors with the move method

--- Crawler.cpp
+++ Crawler.cpp
@@ -30,6 +30,7 @@ void Crawler::move() {
 
     if (isWayBlocked()) {
         Direction newDirection;
+        // keep generating a new direction until it is not the current direction.
         do {
             // https://cplusplus.com/forum/beginner/283229/
             newDirection = (Direction) (1 + rand() % 4);
@@ -37,21 +38,22 @@ void Crawler::move() {
         direction = newDirection;
     }
 
+    // using the min/max to never go beyond the board
     switch (direction) {
         case Direction::NORTH:
-            position.y == std::max(0, position.y - 1);
+            position.y = std::max(0, position.y - 1);
             break;
         case Direction::EAST:
-            position.x == std::max(9, position.x + 1);
+            position.x = std::min(9, position.x + 1);
             break;
         case Direction::SOUTH:
-            position.y == std::max(9, position.y + 1);
+            position.y = std::min(9, position.y + 1);
             break;
         case Direction::WEST:
-            position.x == std::max(0, position.x - 1);
+            position.x = std::max(0, position.x - 1);
             break;
     }
-
+    // record the paths taken
     path.push_back(position);
 }
 
